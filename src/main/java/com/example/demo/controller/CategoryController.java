e35fb13853ee72e63e9610f19f5488f79273223c
Think Dev <hocine.aachiq@gmail.com>
2025-10-18 12:22:39 +0100

    fix controller and jpa config and use service layer & first version api returncatgories finished & WIP other methods

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,15 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
 
 @RestController
 @RequestMapping("/categories") // http://localhost:8080/users
 public class CategoryController {
 	
+	 @Autowired
+	 private CategoryService categoryService;
+	
 	 @GetMapping()
-	 public String getUsers() {
-		 return "get users called";
+	 public List<Category> getCategories() {
+		 return categoryService.getAllCategories();
 	 }
 }
